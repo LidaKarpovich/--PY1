fe73f4233bc613a30ffe3e3370d3eb58f3486d37
LidaKarpovich <94173269+LidaKarpovich@users.noreply.github.com>
2022-11-13 23:55:07 +0300

    Лабораторная работа 5

--- task2.py
+++ task2.py
@@ -11,11 +11,22 @@ def get_count_char(main_str):
     for letter in letter_list:
         letter_dict[letter] = letter_dict.get(letter, default_count) + 1
     return letter_dict
-    print(letter_dict)
 main_str = """
     Данное предложение будет разбиваться на отдельные слова. 
     В качестве разделителя для встроенного метода split будет выбран символ пробела. На выходе мы получим список отдельных слов. 
     Далее нужно отсортировать слова в алфавитном порядке, а после сортировки склеить их с помощью метода строк join. Приступим!!!!
 """
+get_count_char(main_str)
 print(get_count_char(main_str))
+
+def second_f(dict_):
+    dict2 = {}
+    for letter, value in dict_.items():
+        total_value = sum(dict_.values())
+        num_ = round(value / total_value, 2)
+        dict2[letter] = dict2.get(letter, num_)
+    return dict2
+print(second_f(dict_))
+
+
 
